45f29bcd8f08840eb84be783dff779b550f92c70
Rafael K. <rafaelkeitidev@gmail.com>
2024-02-19 22:47:57 -0300

    init: Inicializando projeto

---
+++ src/main/java/com/azuma/messagesapi/MessagesApiApplication.java
@@ -0,0 +1,13 @@
+package com.azuma.messagesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagesApiApplication.class, args);
+	}
+
+}
